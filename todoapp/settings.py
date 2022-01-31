a094a151a792b668e7db2fc41b63dd1b0203e0cd
Christian Baumann <chbauman@student.ethz.ch>
2022-01-31 20:14:38 +0100

    Added 8000 to allowed CORS

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -64,6 +64,8 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "https://chbauman.pythonanywhere.com",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
